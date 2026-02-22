18cee5e184e3e7fc37fd427295e1cc53df6c30a0
PedroPeyroteo <pedropeyroteo@outlook.com>
2026-02-22 16:37:11 +0000

    feat(core): implement toroidal grid with 1D typed storage

---
+++ packages/core/src/gird.ts
@@ -0,0 +1,29 @@
+export class Griid {
+    readonly width: number
+    readonly height: number
+    readonly data: Uint8Array
+
+    constructor(width: number, height: number) {
+        this.width = width
+        this.height = height
+        this.data = new Uint8Array(width * height)
+    }
+
+    index(x: number, y: number): number {
+        const wrappedX = (x + this.width) % this.width
+        const wrappedY = (x + this.height) % this.height
+        return wrappedY * this.width + wrappedX
+    }
+
+    get(x: number, y: number): number {
+        return this.data[this.index(x, y)]
+    }
+
+    set(x: number, y: number, value: number): void {
+        this.data[this.index(x, y)] = value
+    }
+
+    clear(): void {
+        this.data.fill(0)
+    }
+}
