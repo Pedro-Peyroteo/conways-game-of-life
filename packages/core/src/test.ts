3f7a76e09bb4dd3a2205dd05e05947ae7eeaeff1
PedroPeyroteo <pedropeyroteo@outlook.com>
2026-02-22 17:23:43 +0000

    test(core): add basic smoke test for tick progression

---
+++ packages/core/src/test.ts
@@ -0,0 +1,9 @@
+import { Simulation } from "./simulation.js";
+
+const sim = new Simulation(10, 10)
+sim.randomize()
+
+for(let i = 0; i < 5; i++) {
+    sim.step()
+    console.log("Tick: ", sim.tick)
+}
